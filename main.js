931a3192a582576da0987e01c2b43eed9b2dbb47
yukina <ftr.y.1081@gmail.com>
2022-01-28 19:49:55 +0900

    delete-top

--- main.js
+++ main.js
@@ -158,3 +158,18 @@ const animate = () => {
   window.requestAnimationFrame(animate);
 };
 animate();
+
+
+//パーツ
+window.addEventListener ('DOMContentLoaded', ()=>{
+  const btn = document.querySelector('.btn-show');
+  const module = document.querySelector('.module');
+  const closeBtn = document.querySelector('.btn-close');
+  
+  btn.addEventListener("click", () =>{
+   module.classList.remove('hidden');
+  });
+  closeBtn.addEventListener("click", () =>{
+      module.classList.add('hidden');
+     });
+});
